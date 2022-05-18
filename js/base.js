e743c207eae73ce58da94a94f5699f85f08b3e86
Bere <bre_kc073@hotmail.com>
2022-05-18 16:18:16 -0500

    add html and js folder to master

---
+++ js/base.js
@@ -0,0 +1,17 @@
+// console.log('Hola Mundo!');
+
+// Pedir los grados celsius
+
+let gradosCelsius=parseInt(prompt('Introducir la temperatura en tu ciudad en grados Celcius', 28));
+
+//Celsius to fahrenheit 
+// celsius *9/5 +32
+if(gradosCelsius=== null){
+    console.log('La temperatura ingresada no es valida');
+}else{
+    let gradosFahrenheit = ((gradosCelsius*(9/5)) + 32);
+    console.log(`La temperatura en tu ciudad en grados Fahrenheit es ${gradosFahrenheit}`);
+}
+
+// class/class-one master
+// class/class-two clase + tarea
