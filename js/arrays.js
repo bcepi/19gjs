0c95d982c6768fb97688966dfe567e2916f22b73
Bere <bre_kc073@hotmail.com>
2022-05-25 22:58:55 -0500

    add arrays methods file

--- js/arrays.js
+++ js/arrays.js
@@ -12,13 +12,72 @@
 // console.log(newArray)
 
 /**************** Metodos de array ****************
- * 
- * ****arr.push(item): agrega un nuevo ite,m al array al final 
- * 
- * *** arr.pop(item): muta el array original, saca el ultimo elemento 
+ *
+ * ****arr.push(item): agrega un nuevo ite,m al array al final
+ *
+ * *** arr.pop(item): muta el array original, saca el ultimo elemento
  */
 
 // *******************METODO FORHEACH()*****************
 
-let arr = [1,2,3,4,5]
+// let arr = [1,2,3,4,5]
+
+// //arry.foreach( )
+
+// arr.forEach((num, index)=>{
+//     let multiplicacion= num * (index+1)
+//     console.log(`${num} X ${index+1} = ${multiplicacion}`)
+// })
+
+/**
+ * Escribir una funcion
+ * Reciba como parametro Un array de ciudades
+ * Retornar las ciudades capitaslizadas
+ * capitalize(['méxicO','RIo', 'Los AngelEs'])
+ * -> ['México','Rio', 'Los Angeles']
+ *
+ */
+
+// let arrCities = ["méxicO", "RIo", "Los AngelEs"];
+
+// Ejercicio
+
+const capitalizadas = (arr) => {
+  
+    let arrayCapitalizadas = [];
+  
+  arr.forEach((currentValue) => {
+    let firstLetter = currentValue.slice(0, 1).toUpperCase();
+    let secondLetter = currentValue.slice(1).toLowerCase();
+    let word = firstLetter + secondLetter;
+    arrayCapitalizadas.push(word);
+  });
+
+  console.log(arrayCapitalizadas);
+};
+
+// capitalizadas(arrCities);
+
+
+
+
+
+// *******************METODO MAP()*****************
+
+let arrStr = ['hola','mundo']
+
+/**
+ * Funcion
+ * getFirstLast(['hola','mundo'])
+ * -> ['ha','mo']
+ * .map()
+ */
+
+const getFirstLast = (arr) =>{
+    arr.map((str) =>{
+        console.log((`'${str.slice(0,1)}${str.slice(str.length-1)}'`)) 
+    })
+}
+
+getFirstLast(arrStr)
 
