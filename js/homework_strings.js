74a12e7a53143f980a7b9b52aaed78b436dd3ee8
Bere <bre_kc073@hotmail.com>
2022-06-08 16:39:50 -0500

    add notion excercise

--- js/homework_strings.js
+++ js/homework_strings.js
@@ -38,13 +38,76 @@ maskMail('bre_kc073@hotmail.com')
 
 
 
+
+/**
+ * Escribir una funcion, Que dado un string, 
+ * retorne si el string inicia y termina con una vocal
+ * p.ej. 
+ * isVowelSandwich("cat") ➞ true
+ * isVowelSandwich("ear") ➞ false
+ * isVowelSandwich("bake") ➞ false
+ * isVowelSandwich("dog") ➞ true
+
+ * Implementar soluciones con
+ * .slice()
+ * .substring()
+ * .charAt()
+ */
+
+const isVowelSandwich = (word) =>{
+    let firstLetter = word.toLowerCase().slice(0,1)
+    let lastLetter = word.toLowerCase().slice(word.length -1)
+    
+    if (    firstLetter=='a' ||
+            firstLetter =='e' ||
+            firstLetter =='i' ||
+            firstLetter =='o' ||
+            firstLetter =='u' 
+            &&
+            lastLetter=='a' ||
+            lastLetter =='e' ||
+            lastLetter =='i' ||
+            lastLetter =='o' ||
+            lastLetter =='u' ){
+                console.log(true)
+            }
+            else{
+                console.log(false)
+            }
+}
+
+
+
+
+/**
+ * Escribir una funcion, Que dado un string, 
+ * retorne si el string solo contiene letras y espacio
+ * p.ej. 
+ * lettersOnly("JAVAscript") ➞ true
+ * lettersOnly("12321313") ➞ false
+ * lettersOnly("i have spaces") ➞ true
+ * lettersOnly("i have spaces!!!") ➞ false
+
+ * Implementar soluciones con
+ * .test()
+ * .match()
+ * 
+ */
+
+
+
+
 /**
  * Escribir una funcion que convierta un string en formato camel case
  * p.ej
  * -> camelize("JavaScript Exercises") -> "JavaScriptExercises"
+ * 
+ * Implementar soluciones con:
+ * .join()
  */
 
- let ejemplo= "JavaScript Exercises"
+
+ let ejemplo= "Berenice Cervantes Pizana"
 
 const camelize = () =>{ 
     let arrSeparadas = ejemplo.split(' ')
@@ -61,7 +124,7 @@ const camelize = () =>{
         arrCamel.push(word)
     })
 
-    let strCamel= arrCamel.toString().replace(",", "");
+    let strCamel= arrCamel.toString().replace(/,/g, "");
     
     return strCamel
 }
@@ -72,18 +135,17 @@ camelize(ejemplo)
 
 
 
- 
-
-
-
 
 /**
  * Escribir una funcion que convierta de formato camel case a formato normal
  * p.ej
  * -> uncamelize("helloWorld") -> "hello world"
- * -> uncamelize('helloWorld','-') -> "hello-world"
- * -> uncamelize('helloWorld','_') -> "hello_world"
+ * 
+ * Implementar soluciones con:
+ * .forEach()
  */
+
+
 
 
 /**
@@ -92,8 +154,11 @@ camelize(ejemplo)
  * -> repeat('Ha!') -> "Ha!"
  * -> repeat('Ha!', 2) -> "Ha!Ha!"
  * -> repeat('Ha!', 3) -> "Ha!Ha!Ha!"
+ *
+ * Implementar soluciones con:
+ * .repeat()
+ * .forEach()
  */
-
 
 /**
  * Escribir una funcion que cuente las veces que aparece 
@@ -101,5 +166,80 @@ camelize(ejemplo)
  * p.ej
  * -> countWord('Hola mundo', 'mundo') -> 1
  * -> countWord('The quick brown fox jumps over the lazy dog', 'the') -> 2
-
+ *
+ * Implementar soluciones con:
+ * .repeat()
+ * .forEach()
  */
+
+
+
+
+/**
+ * Generar una funcion 
+ * que a partir de un string devuelva solo las iniciales de ese string
+ * getInitialLetters('Hola koders')
+ * -> 'Hk'
+ * 
+ * Implementar soluciones con:
+ * .forEach()
+ * .map()
+ * .reduce()
+ * for()
+ * 
+ */
+
+
+
+
+
+
+/**
+ * Escribir una funcion, Que dado una oracion, 
+ * convierta cada 2 palabras en mayusculas o minusculass
+ * p.ej
+ * swapCase('hola mundo, hola koders')
+ * -> 'hola MUNDO, hola KODERS'
+ * 
+ * Implementar soluciones con:
+ * .map()
+ * .reduce()
+ * .forEach()
+ * for()
+ */
+
+
+
+
+
+/**
+ * Escribir una funcion, Que dado una array de numeros, 
+ * Encuentre el numero mas pequeño
+ * getSmallNumber( [1,5, 7] )
+ * -> 1
+ * 
+ * Implementar soluciones con
+ * for()
+ * .forEach()
+ * .reduce()
+ */
+
+
+
+
+
+
+
+/**
+ * Escribir una funcion, Que dado una array de  10 numeros entre 0 y 9, 
+ * retorne un string en formato telefonico
+ * formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+ * -> "(123) 456-7890"
+ * 
+ * Implementar soluciones con
+ * for()
+ * .forEach()
+ * .reduce()
+ */
+
+
