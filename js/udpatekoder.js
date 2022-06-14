b5b7c09f1ac8c408a4f67ea858934a90c4880415
Bere <bre_kc073@hotmail.com>
2022-06-14 17:39:49 -0500

    add update doc

--- js/udpatekoder.js
+++ js/udpatekoder.js
@@ -1,5 +1,12 @@
+//solicitar http request al navegador 
+//solicitar apertura de la base de datos de farebase, y adicional una comprobacion de la respuesta de la base de datos 
+//proceso de editar el post, maqueta 
+// conectar la maqueta con la base de datos   
+
+
 const ajaxXHR = (callback, url, method = 'GET', obj = {} ) => {
     const xhttp = new XMLHttpRequest()
+    //2.- 
     xhttp.open( method, `https://koders19gjs-default-rtdb.firebaseio.com${url}`, true)
     xhttp.onload = function(data) {
         if(data.target.status >= 200 && data.target.status <= 399){
