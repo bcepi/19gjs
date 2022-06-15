a3fd8d9d2903e67855193070c8b1771c8fe1b198
Bere <bre_kc073@hotmail.com>
2022-06-14 19:59:53 -0500

    add update document

--- js/udpatekoder.js
+++ js/udpatekoder.js
@@ -1,26 +1,59 @@
-//solicitar http request al navegador 
-//solicitar apertura de la base de datos de farebase, y adicional una comprobacion de la respuesta de la base de datos 
-//proceso de editar el post, maqueta 
-// conectar la maqueta con la base de datos   
+// maqueta, proceso de editar el post 
+// sacar los valores de la maqueta
+// enviar cambios al servidor/base de datos   
 
+const koderEditar = (objUpdate) =>{
+  console.log(objUpdate)
+}
 
-const ajaxXHR = (callback, url, method = 'GET', obj = {} ) => {
-    const xhttp = new XMLHttpRequest()
-    //2.- 
-    xhttp.open( method, `https://koders19gjs-default-rtdb.firebaseio.com${url}`, true)
-    xhttp.onload = function(data) {
-        if(data.target.status >= 200 && data.target.status <= 399){
-            let response = JSON.parse(data.target.response)
-            callback(response)
-        }
-    }
-    if(method === 'GET' || method === 'DELETE'){
-        xhttp.send()
-    } else {
-      xhttp.send( JSON.stringify(obj) )
-    }
-  }
+let btnEditar = document.getElementById('editKoder')
+btnEditar.addEventListener('click', () => {
   
+      // obtener los cambios 
+      let name = document.getElementById('nombre').value
+      let age = document.getElementById('edad').value
+      let biography = document.getElementById('biografia').value
+      let bootcamp = document.getElementById('bootcamp').value
 
-// Update Koder
-console.log('SU codigo para editar un koder aquí')
+      console.log(name, age, biography, bootcamp)
+
+      let newKoder = {
+        name: name,
+        age: age,
+        biography: biography,
+        bootcamp: bootcamp
+      }
+  
+      console.log(newKoder)
+
+      // crear el objeto con los datos obtenidos del formulario 
+
+      let idKoder = window.location.search.substring(10)
+      console.log(idKoder)
+  
+    // hacer la edicion
+    const ajaxXHR = (callback, url, method = 'GET', obj = {} ) => {
+      const xhttp = new XMLHttpRequest()
+      xhttp.open( method, `https://koders19gjs-default-rtdb.firebaseio.com${url}`, true)
+      xhttp.onload = function(data) {
+          if(data.target.status >= 200 && data.target.status <= 399){
+              let response = JSON.parse(data.target.response)
+              callback(response)
+          }
+      }
+      if(method === 'GET' || method === 'DELETE'){
+          xhttp.send()
+      } else {
+        xhttp.send( JSON.stringify(obj) )
+      }
+    }
+    
+    ajaxXHR(koderEditar, `/koders/${idKoder}.json`, 'PATCH', newKoder)
+  
+    // 'https://koders19gjs-default-rtdb.firebaseio.com/koders/.json'
+
+    } )
+
+
+
+  
